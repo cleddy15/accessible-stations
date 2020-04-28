a04df82d14b2170270e790fcfe6488a191b44d50
Caroline Leddy <carolineleddy@Carolines-MacBook-Pro.local>
2020-04-28 14:51:20 -0400

    changed one word

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $("body").on("change", ".filter", function (e) {
 	const this_select = e.target;
 	const choice = $(this_select).val();
 
-	$(".item.active").removeClass("active");
-	$(`.item.${choice}`).addClass('active');
+	$(".station.active").removeClass("active");
+	$(`.station.${choice}`).addClass('active');
 });
